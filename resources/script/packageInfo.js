0d7252fd64d65d5f6d1dacbf45f8d5c1463e7fbe
nabilahsan <nabilahsan95@gmail.com>
2018-09-14 23:02:20 -0500

    working js that shows package information

--- resources/script/packageInfo.js
+++ resources/script/packageInfo.js
@@ -1,6 +1,14 @@
 // Clicking on radio button reveals 
 // more information on packages.
 
-$("input[name='test']").click(function () {
-    $('#show-me').css('display', ($(this).val() === 'a') ? 'block':'none');
+$("input[name='MMERGE5']").click(function () {
+
+    //Package 1
+    $('#p1info').css('display', ($(this).val() === 'Package 1') ? 'block':'none');
+
+    //Package 2
+    $('#p2info').css('display', ($(this).val() === 'Package 2') ? 'block':'none');
+
+    //Package 3
+    $('#p3info').css('display', ($(this).val() === 'Package 3') ? 'block':'none');
 });
