2aa9317e015234f20071b19e4bc25925e7aa4350
nabilahsan <nabilahsan95@gmail.com>
2019-02-09 11:22:28 -0600

    added the pro files
    Signed-off-by: nabilahsan <nabilahsan95@gmail.com>

---
+++ resources/script/packageInfo.js
@@ -0,0 +1,17 @@
+// Clicking on radio button reveals 
+// more information on packages.
+
+$("input[name='Package']").click(function () {
+
+    //Package 1
+    $('#p1info').css('display', ($(this).val() === 'Bronze') ? 'block':'none');
+
+    //Package 2
+    $('#p2info').css('display', ($(this).val() === 'Silver') ? 'block':'none');
+
+    //Package 3
+    $('#p3info').css('display', ($(this).val() === 'Gold') ? 'block':'none');
+
+    //Package 4
+    $('#p4info').css('display', ($(this).val() === 'Gold1') ? 'block':'none');
+});
